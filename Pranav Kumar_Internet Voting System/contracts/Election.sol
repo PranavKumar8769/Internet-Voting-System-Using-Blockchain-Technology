b7ccdd536d85efc53a158d458898491813602fdc
Your Name <you@example.com>
2022-05-12 15:14:47 +0530

    first commit

---
+++ Pranav Kumar_Internet Voting System/contracts/Election.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.5.16;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        string party;
+        uint voteCount;
+    }
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Pranav Kumar","Bharatiya Janata Party");
+        addCandidate("Praveen Anand","Indian National Congress");
+        addCandidate("Shubh Shukla","Communist Party Of India");
+        addCandidate("Paras Gera","Bahujan Samaj Party");
+        addCandidate("NOTA","None of the above");
+    }
+
+    function addCandidate (string memory name,string memory party) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name,party, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
+        emit votedEvent(_candidateId);
+    }
+}
